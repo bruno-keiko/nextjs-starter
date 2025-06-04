d84b24bda6c8457aafd745e91327406e45c6520a
Begench <gurbanmammedovbegench07@gmail.com>
2025-06-04 19:44:56 +0500

    feat: language selection

--- src/widgets/language-switcher/index.tsx
+++ src/widgets/language-switcher/index.tsx
@@ -1,16 +1,42 @@
-import { Select, SelectTrigger, SelectValue, SelectContent, SelectItem } from "@/shared/ui/select";
-import React from "react";
+"use client";
+import { useRouter, usePathname } from "@/shared/lib/next-intl/navigaiton";
+import {
+  Select,
+  SelectTrigger,
+  SelectValue,
+  SelectContent,
+  SelectItem,
+  SelectGroup,
+} from "@/shared/ui/select";
+import { Locale, useLocale } from "next-intl";
+import { useParams } from "next/navigation";
+import React, { useTransition } from "react";
 
 const LanguageSwitcher = () => {
+  const router = useRouter();
+  const [isPending, startTransition] = useTransition();
+  const pathname = usePathname();
+  const params = useParams();
+  const locale = useLocale();
+
+  function onSelectChange(value: string) {
+    const nextLocale = value as Locale;
+    startTransition(() => {
+      router.replace({ pathname, query: params }, { locale: nextLocale });
+    });
+  }
+  console.log("Locale switcher");
   return (
-    <Select>
+    <Select defaultValue={locale} onValueChange={onSelectChange} disabled={isPending}>
       <SelectTrigger className="w-[180px]">
-        <SelectValue placeholder="Language" />
+        <SelectValue />
       </SelectTrigger>
       <SelectContent>
-        <SelectItem value="tm">Turkmen</SelectItem>
-        <SelectItem value="ru">Russian</SelectItem>
-        <SelectItem value="en">English</SelectItem>
+        <SelectGroup>
+          <SelectItem value="tk">Türkmen</SelectItem>
+          <SelectItem value="en">English</SelectItem>
+          <SelectItem value="ru">Russian</SelectItem>
+        </SelectGroup>
       </SelectContent>
     </Select>
   );
