2d13bf04e98f4c3bd4a4a5ecdb242da57b0fead9
Begench <gurbanmammedovbegench07@gmail.com>
2025-06-05 17:01:56 +0500

    feat: laguage query

--- src/widgets/language-switcher/index.tsx
+++ src/widgets/language-switcher/index.tsx
@@ -1,5 +1,9 @@
 "use client";
+<<<<<<< HEAD
 import { useRouter, usePathname } from "@/shared/lib/next-intl/navigaiton";
+=======
+import { usePathname, useRouter } from "@/shared/lib/next-intl/navigaiton";
+>>>>>>> 24632a0 (feat: laguage query)
 import {
   Select,
   SelectTrigger,
@@ -9,20 +13,34 @@ import {
   SelectGroup,
 } from "@/shared/ui/select";
 import { Locale, useLocale } from "next-intl";
+<<<<<<< HEAD
 import { useParams } from "next/navigation";
+=======
+import { useSearchParams } from "next/navigation";
+>>>>>>> 24632a0 (feat: laguage query)
 import React, { useTransition } from "react";
 
 const LanguageSwitcher = () => {
   const router = useRouter();
   const [isPending, startTransition] = useTransition();
   const pathname = usePathname();
+<<<<<<< HEAD
   const params = useParams();
   const locale = useLocale();
+=======
+  const params = useSearchParams();
+  const locale = useLocale();
+  const paramsObj = Object.fromEntries(params.entries());
+>>>>>>> 24632a0 (feat: laguage query)
 
   function onSelectChange(value: string) {
     const nextLocale = value as Locale;
     startTransition(() => {
+<<<<<<< HEAD
       router.replace({ pathname, query: params }, { locale: nextLocale });
+=======
+      router.replace({ pathname, query: { ...paramsObj } }, { locale: nextLocale });
+>>>>>>> 24632a0 (feat: laguage query)
     });
   }
   console.log("Locale switcher");
